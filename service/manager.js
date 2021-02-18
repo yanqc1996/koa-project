10b2354b65f14730f566558638ee6c3edc77d9a2
a_mao <yanqinchengyqc@163.com>
2021-02-18 11:52:25 +0800

    update 新增驼峰转换，增加babel

--- service/manager.js
+++ service/manager.js
@@ -1,25 +1,17 @@
 const managerDao = require("../dao/manager")
 const util = require("../utilitys")
+import { jsonToHump } from "am-util"
 /**
- * 用户管理表格查询 TODO:此处考虑下划线转驼峰 mysql建表不能驼峰
+ * 用户管理表格查询
  * @param ctx
  * @returns {Promise<boolean>}
  * @constructor
  */
 exports.queryTable = async (ctx) => {
-  //   let account = ctx.query.account || ""
-  //   let psd = ctx.query.password || ""
-  //   if (!account || !psd) {
-  //     ctx.body = util.resultErrorJson(undefined, "请输入账户或密码信息", {})
-  //     return false
-  //   }
   try {
-    let result = await managerDao.queryTable(ctx)
-    // if (result.length > 0) {
+    // 此处将sql的查询转换为驼峰形式 TODO：将此方法提取到Dao层
+    let result = jsonToHump(await managerDao.queryTable(ctx))
     ctx.body = util.resultSuccessJson(undefined, undefined, result)
-    // } else {
-    //   ctx.body = util.resultSuccessJson(undefined, undefined, {})
-    // }
   } catch (err) {
     ctx.body = util.resultErrorJson(undefined, err, {})
   }
